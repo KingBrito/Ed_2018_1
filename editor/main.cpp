11b7766282e54ec337373f434d794957498226f4
KingBrito <asadesapo@gmail.com>
2018-06-23 12:05:50 -0300

    Update main.cpp
    change a method call ctrl +z and ctrl+ r

--- editor/main.cpp
+++ editor/main.cpp
@@ -2,7 +2,7 @@
 #include <list>
 #include <sstream>
 #include <SFML/Graphics.hpp>
-#include "sftext.h"
+#include "libs/sftext.h"
 
 using namespace std;
 
@@ -11,6 +11,14 @@ struct Ambiente{
     list<char>::iterator cursor;
     Ambiente(){
         cursor = texto.end();
+    }
+    //construtor de cÃ³pia
+    Ambiente(const Ambiente& other):
+        texto(other.texto)
+    {
+        this->cursor = this->texto.begin();
+        for(auto it = other.texto.begin(); it != other.cursor; it++)
+            this->cursor++;
     }
 
     string toString(){
@@ -29,38 +37,55 @@ struct Ambiente{
 int main()
 {
     Ambiente amb;
+    Ambiente ctrlz;
+    Ambiente ctrlr;
+
     amb.texto.push_back('a');
     amb.texto.push_back('m');
     amb.texto.push_back('o');
     amb.texto.push_back('r');
-    list<string>atual;
+    
     sf::RenderWindow janela(sf::VideoMode(800, 600), "Janela");
 
     while(janela.isOpen()){
 
         sf::Event event;
+
         while(janela.pollEvent(event)){
+        	ctrlz.texto.push_back(event.key.code);
+
             if(event.type == sf::Event::Closed)
                 janela.close();
             if(event.type == sf::Event::KeyPressed){
-                if((event.key.code == sf::Keyboard::Z)&&
-                    (amb.cursor != amb.texto.begin())){
-                        std::list<char>::iterator tecla2 = atual.end();
-//                        char* ptrtecla2 = atual.back();
-//                        tecla2 = *ptrtecla2;
-                        amb.texto.insert(amb.cursor,*tecla2);
-                        atual.pop_back();
-                        cout << "control z" << endl;
-                }
-                else if((event.key.code == sf::Keyboard::R)&&
-                    (amb.cursor != amb.texto.begin())){
+                if(event.key.control && (event.key.code == sf::Keyboard::Z)){
+                		cout << "control z" << endl;
+                		
+						if ( ctrlz->texto->head == nullptr){
+							continue;
+						}
+						else{
+						amb.texto.insert(amb.cursor , ctrlz->texto->value);
+						ctrlr.push_back(ctrlz.end());
+						ctrlz.pop_back();
 
-//                        atual.push_back();
-                        cout << "control r" << endl;
+						}
+    			    
                 }
-                else if((event.key.code >= sf::Keyboard::A) &&
+                else if(event.key.control && (event.key.code == sf::Keyboard::R)){
+                        cout << "control r" << endl;
+                        if ( ctrlz->texto->head == nullptr){
+							continue;
+						}
+						else{
+						amb.texto.insert(amb.cursor , ctrlr->texto->value);
+						ctrlz.push_back(ctrlr.end());
+						ctrlr.pop_back();
+
+						}
+                }
+                 else if((event.key.code >= sf::Keyboard::A) &&
                    (event.key.code <= sf::Keyboard::Z)){
-                    char tecla = (event.key.code - sf::Keyboard::A) ;
+                    char tecla = (event.key.code - sf::Keyboard::A) + 'a';
                     cout << tecla << endl;// 'r' e 'z' não esta funcionando
                     amb.texto.insert(amb.cursor,tecla);
                 }
@@ -106,7 +131,4 @@ int main()
     }
     return 0;
 }
-
-
-
 
